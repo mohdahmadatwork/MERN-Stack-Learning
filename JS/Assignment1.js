540292151dbbcb7e50feac4af8e64b50da4bbf31
Mohd. Ahmad <97238445+ahmad9652@users.noreply.github.com>
2023-04-01 13:13:52 +0530

    Added 10 product and made a list of products

--- JS/Assignment1.js
+++ JS/Assignment1.js
@@ -1,1 +1,17 @@
 
+product1={id:1,name:"shoe",price:1000,brand:"sparx"};
+product2={id:2,name:"shirt",price:500,brand:"6teen"};
+product3={id:3,name:"jeans",price:1200,brand:"peter england"};
+product4={id:4,name:"watch",price:700,brand:"fastrack"};
+product5={id:5,name:"t-shirt",price:500,brand:"polo"};
+product6={id:6,name:"shoe",price:1200,brand:"puma"};
+product7={id:7,name:"watch",price:900,brand:"sonata"};
+product8={id:8,name:"socks",price:200,brand:""};
+product9={id:9,name:"mobile",price:10000,brand:"realme"};
+product10={id:10,name:"jeans",price:1900,brand:"levis"};
+Array = [product1,product2,product3, product4, product5, product6, product7,product8,product9,product10];
+While(true){
+console.log(Choose operation);
+console.log(Choose operation);
+console.log(Choose operation);
+}
